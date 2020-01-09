6df3e722a45f7fd5420300b676a89c21200a2ba3
Sebastin Arockia <sebastin.arockia@cred.club>
2020-01-10 00:29:59 +0530

    initial v1

---
+++ src/main/java/in/dreamplug/userservice/MainApplication.java
@@ -0,0 +1,20 @@
+package in.dreamplug.userservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication(scanBasePackages = {"in.dreamplug.userservice"})
+//@EnableJpaRepositories(basePackages = "in.dreamplug.userservice.repository")
+@Slf4j
+public class MainApplication {
+
+    public static void main(String[] args) {
+        log.info("Application starting :::" + LocalDateTime.now());
+        SpringApplication.run(MainApplication.class, args);
+        log.info("Application started :::" + LocalDateTime.now());
+    }
+
+}
---
+++ src/main/java/in/dreamplug/userservice/config/AppConfig.java
@@ -0,0 +1,31 @@
+package in.dreamplug.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+//@EnableRetry
+public class AppConfig {
+
+//	@Bean
+//    RestTemplate restTemplate() {
+//
+//		HttpComponentsClientHttpRequestFactory httpRequestFactory =
+//				new HttpComponentsClientHttpRequestFactory();
+//		httpRequestFactory.setConnectionRequestTimeout(10000);
+//		httpRequestFactory.setConnectTimeout(10000);
+//		httpRequestFactory.setReadTimeout(20000);
+//
+//		return new RestTemplate(httpRequestFactory);
+//	}
+
+
+//	@Bean
+//	@Primary
+//	public ObjectMapper objectMapper(){
+//		ObjectMapper objectMapper = new ObjectMapper();
+//		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+//		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//		return objectMapper;
+//	}
+}
