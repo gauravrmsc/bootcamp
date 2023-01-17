4467ca8e41114421445e50bfb2f1d6a97bf60e3e
Gaurav Kumar <gaurav.kumar1@cred.club>
2023-01-17 20:10:58 +0530

    Project Setup

--- src/main/java/in/dreamplug/userservice/MainApplication.java
+++ src/main/java/in/dreamplug/userservice/MainApplication.java
@@ -1,20 +1,26 @@
 package in.dreamplug.userservice;
 
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDateTime;
-
-@SpringBootApplication(scanBasePackages = {"in.dreamplug.userservice"})
-//@EnableJpaRepositories(basePackages = "in.dreamplug.userservice.repository")
 @Slf4j
+@SpringBootApplication
 public class MainApplication {
-
     public static void main(String[] args) {
         log.info("Application starting :::" + LocalDateTime.now());
         SpringApplication.run(MainApplication.class, args);
         log.info("Application started :::" + LocalDateTime.now());
     }
-
 }
+
+
+
+/*
+ * Spring Actuator : https://docs.spring.io/spring-boot/docs/2.0.0.RELEASE/reference/htmlsingle/#production-ready-endpoints-enabling-endpoints
+ * Important Actuator Endpoints
+ * http://localhost:8082/user-service/actuator/health
+ * http://localhost:8082/user-service/actuator/beans
+ * http://localhost:8082/user-service/actuator/heapdump
+ * */
