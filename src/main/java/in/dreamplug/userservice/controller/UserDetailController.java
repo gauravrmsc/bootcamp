3e315fb194e76ae1063be8441f0b9350ab1ca97c
Gaurav Kumar <gaurav.kumar1@cred.club>
2023-01-17 23:43:54 +0530

    add imports

--- src/main/java/in/dreamplug/userservice/controller/UserDetailController.java
+++ src/main/java/in/dreamplug/userservice/controller/UserDetailController.java
@@ -1,5 +1,6 @@
 package in.dreamplug.userservice.controller;
 
+import java.util.Random;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import in.dreamplug.userservice.dto.UserRequest;
 import in.dreamplug.userservice.entity.User;
-import in.dreamplug.userservice.service.userdetails.IUserDetailService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
