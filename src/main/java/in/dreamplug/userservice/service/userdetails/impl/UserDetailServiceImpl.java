5c75ae402024b3f65346755fe778f6190b25a57d
Gaurav Kumar <gaurav.kumar1@cred.club>
2023-01-18 11:16:35 +0530

    comment out service

--- src/main/java/in/dreamplug/userservice/service/userdetails/impl/UserDetailServiceImpl.java
+++ src/main/java/in/dreamplug/userservice/service/userdetails/impl/UserDetailServiceImpl.java
@@ -16,32 +16,34 @@ import in.dreamplug.userservice.service.userdetails.IUserDetailService;
  * @since 17/01/23
  */
 @Service
-public class UserDetailServiceImpl implements IUserDetailService {
-    private final Map<String, User> users = new HashMap<>();
+public class UserDetailServiceImpl {
 
-    private final Map<String, User> userContactMap = new HashMap<>();
-
-    private final UserMapper userMapper;
-
-    public UserDetailServiceImpl(UserMapper userMapper) {
-        initUsers();
-        this.userMapper = userMapper;
-    }
-
-    @Override
-    public User create(UserRequest userRequest) {
-    }
-
-    @Override
-    public User findByUserId(String userId) {
-    }
-
-    //TODO Create Put Endpoint
-
-    private void initUsers() {
-        users.put("user_id1", User.builder().externalId("user_id1").userName("user1").mobileNumber("8340312345").build());
-        users.put("user_id2", User.builder().externalId("user_id2").userName("user2").mobileNumber("8340312345").build());
-        users.put("user_id3", User.builder().externalId("user_id3").userName("user3").mobileNumber("8340312345").build());
-        users.values().stream().forEach(user -> userContactMap.put(user.getMobileNumber(), user));
-    }
+//        implements IUserDetailService {
+//    private final Map<String, User> users = new HashMap<>();
+//
+//    private final Map<String, User> userContactMap = new HashMap<>();
+//
+//    private final UserMapper userMapper;
+//
+//    public UserDetailServiceImpl(UserMapper userMapper) {
+//        initUsers();
+//        this.userMapper = userMapper;
+//    }
+//
+//    @Override
+//    public User create(UserRequest userRequest) {
+//    }
+//
+//    @Override
+//    public User findByUserId(String userId) {
+//    }
+//
+//    //TODO Create Put Endpoint
+//
+//    private void initUsers() {
+//        users.put("user_id1", User.builder().externalId("user_id1").userName("user1").mobileNumber("8340312345").build());
+//        users.put("user_id2", User.builder().externalId("user_id2").userName("user2").mobileNumber("8340312345").build());
+//        users.put("user_id3", User.builder().externalId("user_id3").userName("user3").mobileNumber("8340312345").build());
+//        users.values().stream().forEach(user -> userContactMap.put(user.getMobileNumber(), user));
+//    }
 }
